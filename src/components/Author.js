b16f11f6acc0cbe9164e13c51a5c8b057b44e5e9
Jeff Henrichs <jeffhenrichs37@gmail.com>
2024-06-15 11:23:00 -0400

    extracting name paramater from URL object

--- src/components/Author.js
+++ src/components/Author.js
@@ -3,10 +3,8 @@ import { useSelector } from "react-redux";
 import { selectAuthors } from "../features/authors/authorsSlice";
 import { Link, useParams } from "react-router-dom";
 
-// Import useParams from React Router
-
 export default function Author() {
-  // Extract the `name` URL parameter.
+  const { name } = useParams();
 
   const authors = useSelector(selectAuthors);
   const author = authors["replace me"];
